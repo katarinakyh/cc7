d3b19ec30094208f96354b53b276a82436759bf5
Katarina <katarina@katarina-VGN-CR35S-L.(none)>
2012-12-10 16:05:21 +0100

    moved theme

--- cc7/apps/account/models.py
+++ cc7/apps/account/models.py
@@ -8,3 +8,39 @@ class MyProfile(UserenaBaseProfile):
                                 unique=True,
                                 verbose_name=_('user'),
                                 related_name='my_profile')
+
+    slug = models.SlugField()
+    description = models.TextField()
+    association = models.ManyToManyField('Association')
+    has_new_message = models.BooleanField(default=False)
+    has_new_comment = models.BooleanField(default=False)
+    
+    def get_message(self):
+        pass
+    
+    def send_message(self, to, text, thread):
+        pass
+    
+    def send_message_to_group(self, to, text, thread):
+        pass
+    
+    def comment(self, post, text):
+        pass
+    
+    
+class Association(UserenaBaseProfile):
+    user = models.OneToOneField(User,
+                                unique=True,
+                                verbose_name=_('user'),
+                                related_name='association')
+
+    slug = models.SlugField()
+    description = models.TextField()
+    
+    def remove_member(self, member):
+        pass
+
+    def remove_event(self, event):
+        pass
+
+    
