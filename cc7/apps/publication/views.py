872b367c52108402d927fcc2697fd8150cebc738
palle torsson <palle.torsson@gmail.com>
2012-12-14 16:32:23 +0100

    början på att skapa poster

--- cc7/apps/publication/views.py
+++ cc7/apps/publication/views.py
@@ -2,7 +2,9 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.core.urlresolvers import reverse
 from models import Post
+from event.models import Event
 from forms import PostForm
+from account.models import MyProfile
 
 class PostView(ListView):
     template_name = 'post/events.html'
@@ -14,9 +16,35 @@ class AddPostView(CreateView):
     form_class = PostForm
 
     def post(self, request, *args, **kwargs):
+        post = Post.objects.creat()
+        title = request.POST.get('title')
+        body = request.POST.get('body')
+        if (request.POST.get('is_public')):
+            is_public = request.POST.get('is_public')
+        if (request.POST.get('personal_page')):
+            personal_page = request.POST.get('personal_page')
+        if (request.POST.get('association')):
+            association = request.POST.get('association')
+        if (request.POST.get('event')):
+            event = request.POST.get('event')
 
-        self.object = None
+        try:
+            post.title = str(title)
+            post.body = str(body)
+            post.author = request.user
+            if (event):
+                post.event = Event.object.get(event=event)
+            if (association):
+                post.association = Association.object.get(association=association)
+            if (personal_page):
+                post.personal_page = MyProfile.object.get(MyProfile=request.user)
+            if (is_public):
+                post.is_public = bool(is_public)
 
+            post.save()
+        except ValueError:
+            pass
+        
         return super(AddPostView, self).post(request, *args, **kwargs)
 
     def get_success_url(self):
