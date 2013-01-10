3682c70d167a949a358edb8906fa9df529dbe70d
Katarina <katarina@katarina-VGN-CR35S-L.(none)>
2013-01-10 20:31:16 +0100

    password change and username at least 2 chars

--- cc7/apps/account/forms.py
+++ cc7/apps/account/forms.py
@@ -1,33 +1,26 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.forms import PasswordChangeForm 
 from userena.forms import EditProfileForm, SignupForm
 from models import MyProfile
 
 attrs_dict = {'class': 'required'}
+USERNAME_RE = r'^[\.e\w]+$'
 
 class EditProfileFormExtra(EditProfileForm):
     class Meta:
         model = MyProfile
         exclude = ('user','slug','privacy','has_new_message','has_new_comment')
         
-USERNAME_RE = r'^[\.e\w]+$'
-
-
+        
 class SignupFormExtra(SignupForm):
-    """
-    Form for creating a new user account.
 
-    Validates that the requested username and e-mail is not already in use.
-    Also requires the password to be entered twice and the Terms of Service to
-    be accepted.
+    def clean_username(self):
+        username  = self.cleaned_data['username']
+        if len(username) < 2 :
+            raise forms.ValidationError("Your username must be at least 2 characters long.")
 
-    """
-    username = forms.RegexField(regex=USERNAME_RE,
-                                min_length=3,
-                                max_length=30,
-                                widget=forms.TextInput(attrs=attrs_dict),
-                                label=_("Username"),
-                                error_messages={'invalid': _('Username must contain only leDDers, numbers, dots and underscores.')})
+        return username
     
     def clean_password1(self):
         password1  = self.cleaned_data['password1']
@@ -35,3 +28,12 @@ class SignupFormExtra(SignupForm):
             raise forms.ValidationError("Password must be at least 5 characters long.")
 
         return password1
+
+
+class PasswordChangeFormExtra(PasswordChangeForm):
+    def clean_new_password1(self):
+        new_password1  = self.cleaned_data['new_password1']
+        if len(new_password1) < 5 :
+            raise forms.ValidationError("Password must be at least 5 characters long.")
+
+        return new_password1
